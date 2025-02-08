ff7572ec4c3e39ec76f762665cc1f3f71a08124a
MiguelAngelMondragonFernandez <20223tn026@utez.edu.mx>
2025-02-08 00:20:53 -0600

    [UPDATE] actualización tarea 04-02-2025 incompleta mensaje 400 no se muestra

--- errorPages/users/views.py
+++ errorPages/users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, authenticate, logout
 from .forms import CustomUserCreationForm, CustomUserLoginForm
 from django.contrib.auth.decorators import login_required
+import json
 
-
+@csrf_exempt
 def register_view(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
@@ -17,15 +19,15 @@ def register_view(request):
 
 
 def login_view(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CustomUserLoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("home")
+            return redirect('home')
     else:
         form = CustomUserLoginForm()
-        return render(request, "login.html", {"form": form})
+    return render(request, 'login.html', {'form': form})
 
 
 def logout_view(request):
